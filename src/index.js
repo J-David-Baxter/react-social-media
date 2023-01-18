5ab44a1c4eb78021fe5fabfb27f47c1add98ec7b
J-David-Baxter <jdavidbx@gmail.com>
2023-01-18 12:13:20 -0500

    Initial set up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
 
-const greeting = <div>Hello React</div>; 
+const rootNode = document.getElementById("root");
+const root = createRoot(rootNode);
+
+root.render(
+    <App />
+);
